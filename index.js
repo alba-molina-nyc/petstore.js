3e50703705ce644c159626d8175f2348f483481b
alba-molina-nyc <avmanyc@gmail.com>
2021-12-26 16:58:03 -0500

    avgfood function on click

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
   });
         
+$('#numAnimalsBtn').click(function(numAnimals, avgFood){
+    var numAnimalsField = $('#numAnimals');
+    var numAnimals = numAnimalsField.val();
+    var avgFood = numAnimals * 3;
+    if (numAnimals <= 0) {
+        console.log(avgFood + 'no need to place order because you said there are 0 or less animals at the moment')
+    }
+    else {
+        $('#calculateFoodOrderResults').show();
+        console.log(avgFood);
+
+    }
+})
